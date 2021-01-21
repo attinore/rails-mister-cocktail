bc29d49568b5cf42f0861a667c81d435b4b4dcb1
Hongjoo Yang <attinore@gmail.com>
2021-01-21 12:20:05 +0900

    review added

---
+++ app/models/review.rb
@@ -0,0 +1,5 @@
+class Review < ApplicationRecord
+  belongs_to :cocktail
+  validates :comment, presence: true
+  validates :rating, presence: true, inclusion: { in: 1..5 }, numericality: { only_integer: true }
+end
